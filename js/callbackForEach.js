e41f88f1ad6b2d0b70740e1736338b6999177c9b
Mayank Kumar <103820518+thismayank1@users.noreply.github.com>
2025-04-22 15:09:41 +0530

    Add files via upload

---
+++ js/callbackForEach.js
@@ -0,0 +1,7 @@
+let nums=[2,4,6,9];
+
+let calcSquare = (num)=>{
+    console.log(num*num);
+};
+
+nums.forEach(calcSquare);
---
+++ js/functionForEach.js
@@ -0,0 +1,5 @@
+let nums=[4,5,8,9];
+
+nums.forEach((num)=>{
+    console.log(num*num);
+})
