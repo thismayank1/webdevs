d3f8aeffeed00e0ef3f20b3cabca0995b0754ce6
Mayank Kumar <103820518+thismayank1@users.noreply.github.com>
2025-04-23 13:34:19 +0530

    Add files via upload

--- DOM/script.js
+++ DOM/script.js
@@ -1,5 +1,4 @@
-let h2 = document.querySelector("h2");
-
-console.dir(h2.innerText);
-
-h2.innerText = h2.innerText + " from Apna College Student";
+let divs = document.querySelectorAll(".box");
+divs[0].innerText="new value 1";
+divs[1].innerText="new value 2";
+divs[2].innerText="new value 3";
